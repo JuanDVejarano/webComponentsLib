c52449996d708a342f0071f84ae090212a72954a
JuanDVejarano <juanjdv_200@hotmail.com>
2023-11-10 16:26:05 -0500

    Fuentes, se quita shadow Dom y se agrega scope al componente

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,8 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface LibButton {
+        "btnSize": string;
+        "btnType": string;
     }
 }
 declare global {
@@ -22,6 +24,8 @@ declare global {
 }
 declare namespace LocalJSX {
     interface LibButton {
+        "btnSize"?: string;
+        "btnType"?: string;
     }
     interface IntrinsicElements {
         "lib-button": LibButton;
--- src/components/lib-button/lib-button.tsx
+++ src/components/lib-button/lib-button.tsx
@@ -1,18 +1,71 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'lib-button',
   styleUrl: './style/lib-button.scss',
-  shadow: true,
+  scoped: true,
+  //shadow: true,
 })
 export class LibButton {
+  @Prop() btnType: string;
+  @Prop() btnSize: string;
+  btnClass: string;
+
   render() {
+    this.selectionClass();
     return (
       <Host>
-        <button type="button" class="btnSecondary">
+        <button type="button" class={this.btnClass}>
           <slot></slot>
         </button>
       </Host>
     );
+  }
+
+  selectionClass() {
+    switch (this.btnType) {
+      case 'primary':
+        switch (this.btnSize) {
+          case 'small':
+            this.btnClass = 'btnPrimary--small';
+            break;
+          case 'large':
+            this.btnClass = 'btnPrimary--medium';
+            break;
+          default:
+            this.btnClass = 'btnPrimary';
+            break;
+        }
+        break;
+      case 'secondary':
+        switch (this.btnSize) {
+          case 'small':
+            this.btnClass = 'btnSecondary--small';
+            break;
+          case 'large':
+            this.btnClass = 'btnSecondary--medium';
+            break;
+          default:
+            this.btnClass = 'btnSecondary';
+            break;
+        }
+        break;
+      case 'neutral':
+        switch (this.btnSize) {
+          case 'small':
+            this.btnClass = 'btnNeutral--small';
+            break;
+          case 'large':
+            this.btnClass = 'btnNeutral--medium';
+            break;
+          default:
+            this.btnClass = 'btnNeutral';
+            break;
+        }
+        break;
+      default:
+        this.btnClass = 'btnPrimary';
+        break;
+    }
   }
 }
